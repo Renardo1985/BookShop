c01334c6e8a2fb0bdb75b0fcb06e6b996ecb232b
Renardo Williams <rennwill@gmail.com>
2023-09-25 21:36:00 -0400

    added models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,61 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    _password_hash= db.Column(db.String())
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    
+    # Define relationships
+    cart_items = db.relationship('Cart', backref='user', lazy=True)
+    addresses = db.relationship('Address', backref='user', lazy=True)
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hash is not accessible")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+class Address(db.Model):
+    __tablename__ = 'address'
+    id = db.Column(db.Integer, primary_key=True)
+    street = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    postal_code = db.Column(db.String(20), nullable=False)
+    country = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    isbn_13 = db.Column(db.String(),unique=True)
+    author = db.Column(db.String(100))
+    price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(100))
+    publisher = db.Column(db.String(100))
+    description = db.Column(db.String())
+    category = db.Column(db.String())
+    
+    # Define relationships to other models
+    cart_items = db.relationship('Cart', backref='book', lazy=True)
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
